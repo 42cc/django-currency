1cdc60de7c67f79a6409bc9c64ea63620dacdabb
Yaroslav Klyuyev <imposeren@gmail.com>
2013-06-18 18:36:15 +0300

    do not override global context

--- currency/models.py
+++ currency/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import datetime
-from decimal import Decimal, getcontext, Context
+from decimal import Decimal, getcontext, Context, setcontext, BasicContext
 
 from django.core.cache import cache
 from django.core.exceptions import ValidationError
@@ -284,13 +284,15 @@ class Money(object):
         if not (isinstance(currency, basestring) and len(currency) == 3):
             raise TypeError("currency argument should be a string with lenght 3")
         self.currency = currency.upper()
-        getcontext().prec = self.precision
+        self.context = Context(prec=self.precision)
+        setcontext(self.context)
         if isinstance(value, Decimal):
             self.value = value
         else:
             self.value = Decimal(str(value))
         self.quantizator = Decimal(10) ** (-places)
         self.value = self.quantize(self.value)
+        setcontext(BasicContext)
         return
 
     def quantize(self, value):
@@ -326,37 +328,50 @@ class Money(object):
         Money
 
         """
-        getcontext().prec = self.precision
+        setcontext(self.context)
         rate = self.get_rate(other_currency)
-        return Money(self.value * rate, other_currency)
+        result = Money(self.value * rate, other_currency)
+        setcontext(BasicContext)
+        return result
 
     def new(self, value):
         """Return new Money instance with same currency but different value
 
         """
-        return Money(value, self.currency)
+        result = Money(value, self.currency)
+        return result
 
     def __add__(self, other):
         self.same_currencies(self, other)
-        getcontext().prec = self.precision
-        return self.new(self.value + other.value)
+        setcontext(self.context)
+        result = self.new(self.value + other.value)
+        setcontext(BasicContext)
+        return result
 
     def __sub__(self, other):
         self.same_currencies(self, other)
         getcontext().prec = self.precision
-        return self.new(self.value - other.value)
+        result = self.new(self.value - other.value)
+        setcontext(BasicContext)
+        return result
 
     def __mul__(self, other):
         getcontext().prec = self.precision
         other = Decimal(str(other))
-        return self.new(self.value * other)
+        result = self.new(self.value * other)
+        setcontext(BasicContext)
+        return result
 
     def __div__(self, other):
         getcontext().prec = self.precision
         other = Decimal(str(other))
-        return self.new(self.value / other)
+        result = self.new(self.value / other)
+        setcontext(BasicContext)
+        return result
 
     def __divmod__(self, other):
         getcontext().prec = self.precision
         other = Decimal(str(other))
-        return self.new(divmod(self.value, other))
+        result = self.new(divmod(self.value, other))
+        setcontext(BasicContext)
+        return result
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-currency',
-    version='0.0.3',
+    version='0.0.4',
     description="simple currency handling for djagngo",
     classifiers=[
         "Programming Language :: Python",
